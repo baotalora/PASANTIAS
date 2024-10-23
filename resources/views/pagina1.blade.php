5dd5688f5e1d7f9e4dc3f10d3df8ab623a678937
Crlosprieto <carlosprieto316@gmail.com>
2024-10-23 11:45:54 -0500

    cristiano

--- resources/views/pagina1.blade.php
+++ resources/views/pagina1.blade.php
@@ -9,6 +9,21 @@
     <!-- Link al archivo de estilos CSS -->
     <link rel="stylesheet" href="{{ asset('css/styles.css') }}">
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <!DOCTYPE html>
+    <html lang="es">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>Zonas Azules - Fusagasugá</title>
+        
+        <!-- Link a los estilos CSS usando Vite -->
+        @vite(['resources/sass/app.scss', 'resources/js/app.js'])  <!-- Carga tu archivo CSS y JS aquí -->
+        
+        <!-- Fuentes de Google y librería de iconos -->
+        <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" rel="stylesheet">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
 
     
     <!-- Fuentes de Google y librería de iconos -->
