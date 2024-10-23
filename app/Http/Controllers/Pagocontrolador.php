3c8f788ba16142d4972a35c10866048e8849a9ce
unknown <baob.brayan2@gmail.com>
2024-10-23 09:44:52 -0500

    nuevos controladores

---
+++ app/Http/Controllers/Pagocontrolador.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class Pagocontrolador extends Controller
+{
+public function index(){
+    return ('Hola como estas ?');
+
+}
+ }
--- routes/web.php
+++ routes/web.php
@@ -2,14 +2,14 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeControlador;
+use App\Http\Controllers\Pagocontrolador;
 use App\Http\Controllers\postcontroller;
+
 
 Route::get('/', [HomeControlador::class,'index' ]);
 
 Route::get('post', [postcontroller::class,'index']);
 
-Route::get('admin', function () {
-    return 'aca seria los codigos qrs';
-});
+Route::get('Pago', [Pagocontrolador::class,'index']);
 
 
